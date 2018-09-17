fd13f1fd614ca1d568adb20ad61174e584bfb4f0
Cambruzzi <32210863+Cambruzzi@users.noreply.github.com>
2018-09-17 13:25:03 -0300

    Update Carta.java

--- SuperTrunfo/Carta.java
+++ SuperTrunfo/Carta.java
@@ -4,30 +4,22 @@ public class Carta{
   Atributo atributos;
 
   public int retornaValor(int numero_atributo){
-    if (numero_atributo == 1 ){
-      return this.atributos.numero_de_albuns;
-    }
-
-    else if (numero_atributo == 2){
-      return this.atributos.numero_de_grammys;
-    }
-
-    else if (numero_atributo == 3){
-      return this.atributos.trocas_de_membros;
-    }
-
-   else if (numero_atributo == 4){
-      return this.atributos.tempo_de_carreira;
-    }
-
-    else if (numero_atributo == 5){
-      return this.atributos.numero_de_albuns_ruins;
-    }
-
-    else {
-      System.out.println("Erro!");
-      return 0;
-    }
+      switch (numero_atributo) {
+          case 1:
+              return this.atributos.numero_de_albuns;
+          case 2:
+              return this.atributos.numero_de_grammys;
+          case 3:
+              return this.atributos.trocas_de_membros;
+          case 4:
+              return this.atributos.tempo_de_carreira;
+          case 5:
+              return this.atributos.numero_de_albuns_ruins;
+          default:
+              System.out.println("Erro!");
+              System.out.println("\n");
+              return 0;
+      }
 
      
   }
